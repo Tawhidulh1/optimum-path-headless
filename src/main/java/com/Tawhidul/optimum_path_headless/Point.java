446f14637915a8bff283f95320619f7cb205dba6
Tawhidul Hasan <Tawhidulh1@gmail.com>
2025-09-19 23:58:03 -0400

    removed redundent point methods

--- src/main/java/com/Tawhidul/optimum_path_headless/Point.java
+++ src/main/java/com/Tawhidul/optimum_path_headless/Point.java
@@ -3,16 +3,10 @@ package com.Tawhidul.optimum_path_headless;
 public class Point {
   private int x;
   private int y;
-  public double f;
-  public double g;
-  private Point parent;
 
   public Point(int x, int y) {
     this.x = x;
     this.y = y;
-    this.f = Double.MAX_VALUE;
-    this.g = Double.MAX_VALUE;
-    parent = null;
   }
 
   public int getX() {
@@ -21,30 +15,6 @@ public class Point {
 
   public int getY() {
     return y;
-  }
-
-  public double getF() {
-    return f;
-  }
-
-  public double getG() {
-    return g;
-  }
-
-  public Point getParent() {
-    return parent;
-  }
-
-  public void setF(double f) {
-    this.f = f;
-  }
-
-  public void setG(double g) {
-    this.g = g;
-  }
-
-  public void setParent(Point parent) {
-    this.parent = parent;
   }
 
   public String toString() {
